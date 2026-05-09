cc9bbe5598e04ae00f8fd050f0c1ebfd6814d098
marken9 <177712014+marken9@users.noreply.github.com>
2026-05-09 21:33:42 +0800

    Finish basic html layout

---
+++ calculator.js
@@ -0,0 +1,17 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+// check for divide by zero in the future
+function divide(a, b) {
+    return a/b;
+}
+
